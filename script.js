0345d8635df8bb813b693bb20ff1e2b3a5ea4eab
axteria <fayee.wu@hotmail.com>
2023-05-14 21:55:11 +1000

    NAV BAR

---
+++ script.js
@@ -0,0 +1,11 @@
+// // HIDE NAV BAR ON SCROLL //
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
